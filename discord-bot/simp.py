494dc6746d751ab7595bd5c181db26d7a23bea5d
Jason Mo <jasonmo0332@gmail.com>
2021-02-13 02:06:46 -0800

    fixed && to and

--- discord-bot/simp.py
+++ discord-bot/simp.py
@@ -31,7 +31,7 @@ async def on_ready():
 async def on_message(message):
 
     sendMessageInt = random.randint(0, 100)
-    if (message.author.id == bkwonUserId && (sendMessageInt < 11 && sendMessageInt > 0)):
+    if (message.author.id == bkwonUserId and (sendMessageInt < 11 and sendMessageInt > 0)):
         randomQuoteInt = random.randint(0, len(simpQuotes)  -  1)
         await message.channel.send(message.author.name + ", " + simpQuotes[randomQuoteInt])
 
